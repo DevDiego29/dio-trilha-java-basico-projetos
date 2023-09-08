adcedb7cad2c53e1670d074f8a90ebd2ad1d1f2b
Diego <adv.diego21@gmail.com>
2023-09-08 07:12:19 -0300

    subindo o projeto DesafioControleFluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+//Essa classe representa a exceção de negócio no sistema
+
+public class ParametrosInvalidosException extends Exception {
+	 public ParametrosInvalidosException(String message) {
+	        super(message);
+	    }
+}
